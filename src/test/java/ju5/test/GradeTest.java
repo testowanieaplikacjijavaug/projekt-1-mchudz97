f52914403057caa56999ab5764c05d9d1c60c152
Maciej Chudzinski <mchudz97@gmail.com>
2020-04-03 11:47:50 +0200

    of method test

---
+++ src/test/java/ju5/test/GradeTest.java
@@ -0,0 +1,33 @@
+package ju5.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class GradeTest {
+
+
+
+
+
+    @ParameterizedTest(name = "{index} Parameter {0} throws argument exception")
+    @DisplayName("Grade class of method test if invalid value")
+    @CsvSource("0, 10, -3, 1, 2.25, 5.5")
+    public void gradeOfMethodTestException(String val){
+
+        Assertions.assertThrows(new IllegalArgumentException("Grade value is out of range."),
+                () -> Grade.of(Float.parseFloat(val)));
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/ju5/test/StudentTest.java
@@ -0,0 +1,12 @@
+package ju5.test;
+
+import org.junit.jupiter.api.BeforeAll;
+
+public class StudentTest {
+
+    public Student studentTest
+
+
+
+
+}
