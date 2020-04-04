8f18cf1a17e7c590de4bd986bbd21465d704c2a9
Maciej Chudzinski <mchudz97@gmail.com>
2020-04-04 20:39:39 +0200

    Note of  method added

--- src/main/java/SchoolSystem/Note.java
+++ src/main/java/SchoolSystem/Note.java
@@ -1,6 +1,25 @@
 package SchoolSystem;
 
 public class Note implements INote {
+
+    private String description;
+
+    public static Note of(String desc){
+
+        if(desc == null && desc == ""){
+
+            throw new IllegalArgumentException("fescription got blank or null value!");
+
+        }
+
+        return new Note(desc);
+
+    }
+
+    private Note(String desc){
+        this.description = desc;
+    }
+
     @Override
     public String readNote() {
         return null;
