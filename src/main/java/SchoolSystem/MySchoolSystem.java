56f59facf2d71fee3fd9f6bf7e56119a8cba41a2
Maciej Chudzinski <mchudz97@gmail.com>
2020-04-04 14:41:04 +0200

    SchoolSystem addStudent test with exception

--- src/main/java/SchoolSystem/MySchoolSystem.java
+++ src/main/java/SchoolSystem/MySchoolSystem.java
@@ -1,4 +1,26 @@
 package SchoolSystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MySchoolSystem {
+
+    public List<Student> students;
+    public List<ISchoolSubject> schoolSubjects;
+
+    public MySchoolSystem(){
+
+        this.students = new ArrayList<Student>(0);
+        this.schoolSubjects = new ArrayList<ISchoolSubject>(0);
+
+
+    }
+
+    public void addStudent(Student st){
+
+
+
+    }
+
+
 }
--- src/test/java/ju5/test/SchoolSystemTest.java
+++ src/test/java/ju5/test/SchoolSystemTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.mockito.Mockito.when;
+
 public class SchoolSystemTest {
 
     private MySchoolSystem ss;
@@ -22,7 +24,7 @@ public class SchoolSystemTest {
 
 
     @Test
-    @DisplayName("")
+    @DisplayName("simple add student MySchoolSystem test")
     public void addStudentTest(){
 
         Student st = Mockito.mock(Student.class);
@@ -32,6 +34,22 @@ public class SchoolSystemTest {
         Assertions.assertEquals(st, ss.students.get(st));
 
 
+
+
+    }
+    @Test
+    @DisplayName("Student index must be unique. In other case it will throw exception")
+    public void addStudentWithException(){
+
+        Student st1 = Mockito.mock(Student.class);
+        Student st2 = Mockito.mock(Student.class);
+        when(st1.getIndex()).thenReturn("111111");
+        when(st2.getIndex()).thenReturn("111111");
+
+        ss.addStudent(st1);
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> ss.addStudent(st2));
 
 
     }
